1545be01d9cff0b02eea6997958c1e410d91d516
mahmudur987 <safemahmud987@agmail.com>
2023-11-08 14:30:36 +0600

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import Navbar from "@/components/shared/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import SideNav from "@/components/shared/SideNav";
-import img1 from "@/assets/images/food-network 1.png";
 import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,11 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <link
-          rel="icon"
-          href="http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRq1mQLx2tcZ4owspN_OxqcHZBIc0f1F6NfiqBYgGnwEkdc3tbUGKETWmBH2PNppLs-dJJAt92vs2I16Eno7ks"
-          sizes="any"
-        />
+        <link rel="icon" href="./favicon.ico" sizes="any" />
       </Head>
 
       <body className={inter.className}>
